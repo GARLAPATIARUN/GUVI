a3a6c46b8b9a4d8bed46f5dcdde667f371c9584c
Arun Garlapati <arunchamp400@gmail.com>
2019-07-17 19:49:18 +0530

    Add files via upload

---
+++ prime_number_verfication.py
@@ -0,0 +1,11 @@
+number=int(input())
+if number>1 and number<1000:
+    for i in range(2,number):
+        if(number%i==0):
+            print("no")
+            break
+        else:
+            print("yes")
+else:
+    print("no")
+        
